d9ee3e1d9a260d99174f51e568fea7dccca162d0
zafoddiy <seppelias1@gmail.com>
2024-10-14 00:32:46 +0000

    Task 4.2 additions

--- setup.py
+++ setup.py
@@ -25,8 +25,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-                'walker = ias0220_231899.random_walker:main',
-                'position_calculator = ias0220_231899.odometer:main',
+                'odometry = ias0220_231899.odometer:main',
         ],
     },
 )
