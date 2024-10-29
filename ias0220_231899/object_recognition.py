74476fa2b109652fc8b544607cc23eed3ee59121
zafoddiy <seppelias1@gmail.com>
2024-10-29 15:08:26 +0200

    Task 5.2 final

--- ias0220_231899/object_recognition.py
+++ ias0220_231899/object_recognition.py
@@ -7,13 +7,14 @@ from ament_index_python.packages import get_package_share_directory
 
 from sensor_msgs.msg import Image
 
+
 class ObjectsNodes(Node):
 
     def __init__(self):
         super().__init__('object_recognition')
 
         self.bridge = CvBridge()
-        
+
         self.object_subscription = self.create_subscription(
             Image,
             '/camera1/image_raw',
@@ -21,19 +22,33 @@ class ObjectsNodes(Node):
             10)
         self.debug_publisher = self.create_publisher(
             Image,
-            '/debug_image',
+            '/contoured_image',
             10)
         self.i = 0
-    
+
     def subscriber_callback(self, imageMsg):
-        img = self.bridge.imgmsg_to_cv2(imageMsg)
-        self.get_logger().info("Starting process")
+        img = self.bridge.imgmsg_to_cv2(imageMsg, desired_encoding='bgr8')
         img_HSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-        lower_blue = np.array([-10,100,100])
-        upper_blue = np.array([10,255,255])
-        img_masked = cv.inRange(img_HSV, lower_blue, upper_blue)
-        result = cv.bitwise_and(img,img, mask= img_masked)
-        image_debug_msg = self.bridge.cv2_to_imgmsg(result)
+        lower_red = np.array([-10, 100, 100])
+        upper_red = np.array([5, 255, 255])
+        img_masked = cv.inRange(img_HSV, lower_red, upper_red)
+        contours, hierarchy = cv.findContours(
+            img_masked, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+        contours_poly = [None]*len(contours)
+        centers = [None]*len(contours)
+        radius = [None]*len(contours)
+        for i, c in enumerate(contours):
+            contours_poly[i] = cv.approxPolyDP(c, 3, True)
+            centers[i], radius[i] = cv.minEnclosingCircle(contours_poly[i])
+
+        try:
+            cv.circle(img, (int(centers[1][0]), int(
+                centers[1][1])), int(radius[1]), (0, 255, 0), 2)
+        except Exception as e:
+            self.get_logger().info("No object detected")
+        image_debug_msg = self.bridge.cv2_to_imgmsg(
+            img, encoding='bgr8')
         self.get_logger().info("Publishing message")
         self.debug_publisher.publish(image_debug_msg)
 
--- launch/task5_part2_231899.launch.py
+++ launch/task5_part2_231899.launch.py
@@ -10,12 +10,14 @@ import xacro
 
 package_name = "ias0220_231899"
 
+
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='True')
     package_path = os.path.join(get_package_share_directory(package_name))
 
     # Parse the urdf with xacro
-    xacro_file = os.path.join(package_path, "urdf", "differential_robot_simu_task5_part2.urdf")
+    xacro_file = os.path.join(package_path, "urdf",
+                              "differential_robot_simu_task5_part2.urdf")
     doc = xacro.parse(open(xacro_file))
     xacro.process_doc(doc)
     params = {"robot_description": doc.toxml(), "use_sim_time": use_sim_time}
@@ -45,33 +47,34 @@ def generate_launch_description():
         parameters=[params],
     )
 
-    #image_pub_node = Node(
+    # image_pub_node = Node(
     #    package="ias0220_231899",
     #    executable="image_publisher",
     #    name="image_publisher",
     #    output="screen"
-    #)
+    # )
 
-    #camera_calib_node = Node(
+    # camera_calib_node = Node(
     #    package="ias0220_231899",
     #    executable="camera_calibration",
     #    name="camera_calibration",
     #    output="screen"
-    #)
+    # )
 
-    #static_transform_publisher_node = Node(
+    # static_transform_publisher_node = Node(
     #    package="tf2_ros",
     #    executable="static_transform_publisher",
     #    arguments = ["--frame-id", "map", "--child-frame-id", "odom"],
-    #)
+    # )
 
     gazebo_spawn_entity_node = Node(
         package="gazebo_ros",
         executable="spawn_entity.py",
-        name="differential_robot_simu_task4_part2",
+        name="differential_robot_simu_task5_part2",
         namespace="my_robot",
         output="screen",
-        arguments=["-topic", "/robot_description", "-entity", "observerbot", "-x", "0.6", "-y", "-7.5", "-Y", "1.6"],
+        arguments=["-topic", "/robot_description", "-entity",
+                   "observerbot", "-x", "0.6", "-y", "-7.5", "-Y", "1.6"],
     )
 
     object_recognition_node = Node(
@@ -90,13 +93,14 @@ def generate_launch_description():
         robot_state_publisher_node,
         gazebo_spawn_entity_node,
         object_recognition_node,
-        #IncludeLaunchDescription(
+        # IncludeLaunchDescription(
         #    PythonLaunchDescriptionSource([get_package_share_directory('setup_gazebo_ias0220'), '/launch/gazebo.launch.py']),
         #    launch_arguments={
         #        'xacro_file': xacro_file,
         #    }.items()
-        #),
+        # ),
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([get_package_share_directory('machine_vision_part2'), '/launch/mvt_main.launch.py']),
-        )             
+            PythonLaunchDescriptionSource([get_package_share_directory(
+                'machine_vision_part2'), '/launch/mvt_main.launch.py']),
+        )
     ])
